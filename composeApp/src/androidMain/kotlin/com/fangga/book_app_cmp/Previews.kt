2a696c8dcca741555ae965a5bcd47d03e9bb76e1
Fa'iq Arya Dewangga <fanggadewangga@gmail.com>
2025-03-13 21:36:28 +0700

    feat(home): add book list screen

---
+++ composeApp/src/androidMain/kotlin/com/fangga/book_app_cmp/Previews.kt
@@ -0,0 +1,44 @@
+package com.fangga.book_app_cmp
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.fangga.book_app_cmp.books.presentation.book_list.BookListScreen
+import com.fangga.book_app_cmp.books.presentation.book_list.BookListScreenRoot
+import com.fangga.book_app_cmp.books.presentation.book_list.BookListState
+import com.fangga.book_app_cmp.books.presentation.book_list.components.BookSearchBar
+
+@Preview(backgroundColor = 0xFFFFFF)
+@Composable
+private fun BookSearchBarPreview() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White),
+        ) {
+            BookSearchBar(
+                searchQuery = "Kotlin",
+                onSearchQueryChange = {},
+                onImeSearch = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BookScreenPreview() {
+    MaterialTheme {
+        BookListScreen(
+            state = BookListState(),
+
+        ) { }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/fangga/book_app_cmp/books/presentation/book_list/BookListState.kt
@@ -0,0 +1,29 @@
+package com.fangga.book_app_cmp.books.presentation.book_list
+
+import com.fangga.book_app_cmp.books.domain.Book
+import com.fangga.book_app_cmp.core.presentation.UiText
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = books,
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)
+
+val books = (1..100).map { it ->
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = it.toString(),
+        authors = listOf("tes"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishedYear = null,
+        averageRating = 4.6789,
+        ratingCount = 5,
+        numPages = it,
+        numEditions = it,
+    )
+}
